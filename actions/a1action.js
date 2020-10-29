8a811296126b098c3142c9e2e5ff23b20db7ae5d
wlionhart <71235693+wlionhart@users.noreply.github.com>
2020-10-29 00:56:09 -0700

    Add files via upload

---
+++ actions/a1action.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    var x = document.getElementById("mtnav");
+    if (x.className === "tnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "tnav";
+    }
+}
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
